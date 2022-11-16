3cc06e9eb5f89fd6dc35eea92309a1ee19a3ad2d
Amaury Rodrigo <amaury.soares@madeiramadeira.com.br>
2022-11-16 20:16:36 -0300

    fix relative path

--- src/services/headlines.js
+++ src/services/headlines.js
@@ -1,7 +1,7 @@
 const apiUrl = process.env.HEADLINESBR_API_URL
 
 export const getPosts = async (page = 1, perPage = 25) => {
-  const response = await fetch(apiUrl + `?page=${page}&per_page=${perPage}`)
+  const response = await fetch(`http://191.252.103.213:8090/?page=${page}&per_page=${perPage}`)
   const data = await response.json()  
   return data.model
 }
